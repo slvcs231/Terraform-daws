9e006c5fd23ac04a35d4326e897a327f9723efec
Reddeiah <reddeiah18g@gmail.com>
2026-09-18 13:51:02 +0530

    first commit

--- condition/ec2.tf
+++ condition/ec2.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "allow_all_cond" {
         from_port = 0
         to_port = 0
         protocol = "-1"
-        cidr_blocks = [0.0.0.0/0]
+        cidr_blocks = ["0.0.0.0/0"]
         
     }
   
