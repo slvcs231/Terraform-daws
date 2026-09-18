8c42484223c4277261b6e38dd9e0b871b92028ea
Reddeiah <reddeiah18g@gmail.com>
2026-09-18 16:26:13 +0530

    first commit

---
+++ count/ec2.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "terraform_count" {
+  ami = "ami-0220d79f3f480ecf5"
+  count = 3
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws.aws_security_group.allow_all_count.id]
+
+  tags = {
+    Name = "terraform_count"
+    Terraform = "True"
+  }
+}
+
+resource "aws_security_group" "allow_all_count" {
+  name = "allow_all"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+}
