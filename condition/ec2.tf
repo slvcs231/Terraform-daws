d7ad69d2d33c0ce01835381ef538659c5cd1ac67
Reddeiah <reddeiah18g@gmail.com>
2026-09-18 13:22:39 +0530

    first commit

---
+++ condition/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "Terraform-1" {
+  ami = "ami-0220d79f3f480ecf5"
+  instance_type = var.environment == "dev" ? "t3.micro" : "t3.small"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  tags = {
+    Name      = "Terraform1"
+    Terraform = "True"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+    name = "allow-all"
+
+    egress {
+        from_port = 0
+        to_port   = 0
+        protocol  = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
+    ingress { 
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = [0.0.0.0/0]
+        
+    }
+  
+}
