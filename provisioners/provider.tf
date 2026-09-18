ad9cd6f826138196fa1035305cda31b08ad5a985
Reddeiah <reddeiah18g@gmail.com>
2026-09-18 23:06:16 +0530

    first commit

--- provisioners/provider.tf
+++ provisioners/provider.tf
@@ -1,19 +1,19 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "6.16.0"
+      source  = "hashicorp/aws"
+      version = "6.16.0"
     }
   }
-}
 
- backend "s3" {
-    bucket = "remote-state-86s-dev1"
-    key    = "remote-state-86s-dev1-demo"
-    region = "us-east-1"
+  backend "s3" {
+    bucket       = "remote-state-86s-dev1"
+    key          = "remote-state-86s-dev1-demo"
+    region       = "us-east-1"
     use_lockfile = true
-    encrypt = true
+    encrypt      = true
   }
+}
 
 provider "aws" {
   region = "us-east-1"
